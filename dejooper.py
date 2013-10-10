38d3206572a57ce98f7abf69e6a1b45aac7eb707
Joe Drumgoole <joe@joedrumgoole.com>
2013-10-10 10:33:31 +0100

    removed some junk

--- dejooper.py
+++ dejooper.py
@@ -93,18 +93,6 @@ def scanFiles( filesCollection, d, upload, host ):
     return ( count, timing.elapsed())
 
 
-
-    
-class CLIError(Exception):
-    '''Generic exception to raise and log different fatal errors.'''
-    def __init__(self, msg):
-        super(CLIError).__init__(type(self))
-        self.msg = "E: %s" % msg
-    def __str__(self):
-        return self.msg
-    def __unicode__(self):
-        return self.msg
-
 def main(argv=None):
     '''Command line options.'''
     
@@ -112,28 +100,10 @@ def main(argv=None):
         argv = sys.argv
     else:
         sys.argv.extend(argv)
-
-    program_name = os.path.basename(sys.argv[0])
-    program_version = "v%s" % __version__
-    program_build_date = str(__updated__)
-    program_version_message = '%%(prog)s %s (%s)' % (program_version, program_build_date)
-    program_shortdesc = __import__('__main__').__doc__.split("\n")[1]
-    program_license = '''%s
-
-  Created by Joe.Drumgoole@10gen.com on %s.
-  
-  Licensed under the Apache License 2.0
-  http://www.apache.org/licenses/LICENSE-2.0
-  
-  Distributed on an "AS IS" basis without warranties
-  or conditions of any kind, either express or implied.
-
-USAGE
-''' % (program_shortdesc, str(__date__))
-
+        
     try:
         # Setup argument parser
-        parser = ArgumentParser(description=program_license, formatter_class=RawDescriptionHelpFormatter)
+        parser = ArgumentParser(description="dejooper 0.1beta", formatter_class=RawDescriptionHelpFormatter)
         
         parser.add_argument( "-s", "--scan", 
                              dest="scandir", 
